c9221ef9418193a78495928de6515cd32fc34fd5
Arthur Morin <arthur.morin034@gmail.com>
2025-12-09 10:07:55 +0100

    script et extra

--- js/script.js
+++ js/script.js
@@ -1,28 +1,6 @@
-(function() {
-  // Fonction utilitaire pour gérer une image
-  const handleImageLoad = (img) => {
-    img.classList.add('loaded');
-  };
-
-  // Logique principale
-  const initImages = () => {
+(function(){
+  window.addEventListener('load', function(){
     const imgs = document.querySelectorAll('.card img');
-    
-    imgs.forEach(img => {
-      // Si l'image est déjà dans le cache du navigateur
-      if (img.complete) {
-        handleImageLoad(img);
-      } else {
-        // Sinon, on attend qu'elle charge
-        img.addEventListener('load', () => handleImageLoad(img));
-      }
-    });
-  };
-
-  // On lance le script quand le DOM est prêt (plus rapide que window.load)
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initImages);
-  } else {
-    initImages();
-  }
+    imgs.forEach(img => { if (img.complete) img.classList.add('loaded'); else img.addEventListener('load', ()=> img.classList.add('loaded')); });
+  });
 })();
